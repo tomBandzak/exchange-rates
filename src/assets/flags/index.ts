47d8d2efaf307030d0c796fba4dfb0c4b604fb04
RD_dev <TzJBKTvQ7cTwuu8T>
2023-11-04 23:24:36 +0100

    Add country flags

---
+++ src/assets/flags/index.ts
@@ -0,0 +1,67 @@
+import AUD from './AUD.png';
+import BGN from './BGN.png';
+import BRL from './BRL.png';
+import CAD from './CAD.png';
+import CHF from './CHF.png';
+import CNY from './CNY.png';
+import DKK from './DKK.png';
+import EUR from './EUR.png';
+import GBP from './GBP.png';
+import HKD from './HKD.png';
+import HUF from './HUF.png';
+import IDR from './IDR.png';
+import ILS from './ILS.png';
+import INR from './INR.png';
+import ISK from './ISK.png';
+import JPY from './JPY.png';
+import KRW from './KRW.png';
+import MXN from './MXN.png';
+import MYR from './MYR.png';
+import NOK from './NOK.png';
+import NZD from './NZD.png';
+import PHP from './PHP.png';
+import PLN from './PLN.png';
+import RON from './RON.png';
+import SEK from './SEK.png';
+import SGD from './SGD.png';
+import THB from './THB.png';
+import TRY from './TRY.png';
+import USD from './USD.png';
+import XDR from './XDR.png';
+import ZAR from './ZAR.png';
+
+const flags: {[key: string]: any} = {
+  AUD,
+  BGN,
+  BRL,
+  CAD,
+  CHF,
+  CNY,
+  DKK,
+  EUR,
+  GBP,
+  HKD,
+  HUF,
+  IDR,
+  ILS,
+  INR,
+  ISK,
+  JPY,
+  KRW,
+  MXN,
+  MYR,
+  NOK,
+  NZD,
+  PHP,
+  PLN,
+  RON,
+  SEK,
+  SGD,
+  THB,
+  TRY,
+  USD,
+  XDR,
+  ZAR
+}
+
+export default flags;
--- src/components/CurrenciesTable.tsx
+++ src/components/CurrenciesTable.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { CurrencyRow } from '../functions/parseCurrencyRow';
 import { Col, HeadCol, Row, Table } from '../styled';
+import flags from '../assets/flags';
 
 type Props = {
   rates: CurrencyRow[];
@@ -9,6 +10,7 @@ type Props = {
 export const CurrenciesTable = ({ rates }: Props) => <Table>
   <thead>
     <tr>
+      <HeadCol />
       <HeadCol width={'50%'}>Country</HeadCol>
       <HeadCol width={'12%'} minwidth={'90px'}>Currency</HeadCol>
       <HeadCol>Amount</HeadCol>
@@ -17,6 +19,6 @@ export const CurrenciesTable = ({ rates }: Props) => <Table>
     </tr>
   </thead>
   <tbody>
-  {rates.map(rate => <Row key={rate.currencyCode}>{Object.values(rate).map((col, j) => <Col key={`col-${j}`}>{col}</Col>)}</Row>)}
+  {rates.map(rate => <Row key={rate.currencyCode}><Col><img alt={'flag'} height={35} src={flags[rate.currencyCode]}/></Col>{Object.values(rate).map((col, j) => <Col key={`col-${j}`}>{col}</Col>)}</Row>)}
   </tbody>
 </Table>
