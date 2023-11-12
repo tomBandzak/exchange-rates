77a834d0f39571f948b56a0dc2c79845d3d7bf07
RD_dev <TzJBKTvQ7cTwuu8T>
2023-11-12 23:58:41 +0100

    Remove useEffect and move parsing to fetching function. We are loading only once, no need to react to data change.

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { CurrenciesTable, ExchangeCalculator } from './components';
-import { CurrencyRow, parseCurrencyRow } from './functions/parseCurrencyRow';
 import { getExchangeRates } from './request/ratesRequest';
 import { QueryClient, useQuery } from '@tanstack/react-query';
 import { Box, Description, Page, XLargeHeading } from './styled';
 import { pictures } from './assets/images';
 
 function App() {
-  const [currencyList, setCurrencyList] = useState<CurrencyRow[]>([]);
   const [exchangedCurrency, setExchangedCurrency] = useState<string | undefined>(undefined);
-  const [dateString, setDateString] = useState<string>('');
 
   const queryClient = new QueryClient();
   const { data, isLoading, error } = useQuery({ queryKey: ['exchangeRates'], queryFn: getExchangeRates }, queryClient);
-
-  useEffect(() => {
-    if (!isLoading && !error && data) {
-      const rates = data.split('\n').slice(2).filter(Boolean).map(row => parseCurrencyRow(row));
-      setDateString(data.split('\n')[0].split('#')[0]);
-      setCurrencyList(rates);
-      setExchangedCurrency(rates[0].currencyCode)
-    }
-  }, [data]);
 
   if (isLoading) {
     return <>
@@ -35,17 +23,17 @@ function App() {
     <Page>
       <div>
         <XLargeHeading>Czech crowns conversion calculator</XLargeHeading>
-        {error ? <>
+        {error || !data ? <>
             <div>Ooops... Something went wrong</div>
             <img src={pictures.sleepy} alt={'sleepy-dog'} />
           </> :
           <>
-            <h3>Date {dateString}</h3>
+            <h3>Date {data.date}</h3>
             <Box>
               <Description>Find out the amount of Czech crowns converted to foreign currency</Description>
-              {!!currencyList.length
-              && <ExchangeCalculator currencies={currencyList} exchangedCurrency={exchangedCurrency} setExchangedCurrency={setExchangedCurrency}/>}
-              <CurrenciesTable rates={currencyList} exchangedCurrency={exchangedCurrency} setExchangedCurrency={setExchangedCurrency} />
+              {!!data.currencies.length
+              && <ExchangeCalculator currencies={data.currencies} exchangedCurrency={exchangedCurrency || data.currencies[0].currencyCode} setExchangedCurrency={setExchangedCurrency}/>}
+              <CurrenciesTable rates={data.currencies} exchangedCurrency={exchangedCurrency || data.currencies[0].currencyCode} setExchangedCurrency={setExchangedCurrency} />
             </Box>
           </>}
       </div>
--- src/request/ratesRequest.ts
+++ src/request/ratesRequest.ts
@@ -1,9 +1,19 @@
-export const getExchangeRates = async () => {
+import { CurrencyRow, parseCurrencyRow } from '../functions/parseCurrencyRow';
+
+type ParsedData = {
+  date: string;
+  currencies: CurrencyRow[];
+}
+
+export const getExchangeRates = async (): Promise<ParsedData | undefined> => {
   const resp = await fetch('https://cors-proxy-node.vercel.app/api?url=https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt');
-  console.log(resp);
   if(resp.ok) {
-    return await resp.text();
+    const textData = await resp.text();
+    return {
+      date: textData.split('\n')[0].split('#')[0],
+      currencies: textData.split('\n').slice(2).filter(Boolean).map(row => parseCurrencyRow(row))
+    };
   }
 
-  return '';
+  return undefined;
 }
